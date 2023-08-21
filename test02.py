157bda20da11fb98dd3eb82d688b763833a60a74
szw <3198734288@qq.com>
2023-08-21 17:13:11 +0800

    add

--- test02.py
+++ test02.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2 as cv
+print('yes')
 img=cv.imread('sample.jpg',1)
 cv.namedWindow('111',cv.WINDOW_NORMAL)
 cv.imshow('111',img)
 res = cv.resize(img,None,fx=2, fy=2, interpolation = cv.INTER_CUBIC)
 
 cv.waitKey(0)
-print('yes')
 cv.destroyAllWindows()
